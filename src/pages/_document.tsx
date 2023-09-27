0dee850e5991d11b971851cbcae05c7258fbc3aa
miaslls <97785569+miaslls@users.noreply.github.com>
2023-09-27 18:03:45 -0300

    further remove unused + prettier format

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
@@ -13,7 +13,6 @@ export default function Document() {
           content="Task List, Task Management, Add Tasks, Update Tasks, Delete Tasks, Complete Tasks, Stay Organized, Boost Productivity, Task Tracker, To-Do List, Task Organizer, Web App"
         />
         <meta name="author" content="miaslls" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="shortcut icon" href="img/logo.svg" type="image/svg+xml" />
         <link rel="manifest" href="manifest.json" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>
-          MiaTask - Simple Task Management | Organize, Update, and Complete
-          Tasks Effortlessly
+          MiaTask - Simple Task Management | Organize, Update, and Complete Tasks Effortlessly
         </title>
       </Head>
 
