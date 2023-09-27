af79cdb7436373a9fbdc0710b8229f2a0986433a
miaslls <97785569+miaslls@users.noreply.github.com>
2023-09-27 16:34:51 -0300

    meta tags + metadata files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Effortless Task Management. Stay Organized and Boost Productivity with this Simple Task List Web App."
+        />
+        <meta
+          name="keywords"
+          content="Task List, Task Management, Add Tasks, Update Tasks, Delete Tasks, Complete Tasks, Stay Organized, Boost Productivity, Task Tracker, To-Do List, Task Organizer, Web App"
+        />
+        <meta name="author" content="miaslls" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="shortcut icon" href="img/logo.svg" type="image/svg+xml" />
+        <link rel="manifest" href="manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>
+          MiaTask - Simple Task Management | Organize, Update, and Complete
+          Tasks Effortlessly
+        </title>
       </Head>
 
-      <p>Hello, World!</p>
+      <p>Hello, Next.js!</p>
     </>
   );
 }
