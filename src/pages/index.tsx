0d3bd16dba4d04f2e300477ff3fa5c1347ca2500
miaslls <97785569+miaslls@users.noreply.github.com>
2023-09-28 17:13:49 -0300

    style footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/home.module.css';
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,21 +10,52 @@ export default function Home() {
           MiaTask - Simple Task Management | Organize, Update, and Complete Tasks Effortlessly
         </title>
       </Head>
-      <header>
-        <div className={styles.title}>
-          <div className={styles.title_logo}>
-            <i className="ri-list-check-3"></i>
+      <div className="outer_container">
+        <header>
+          <div className={styles.title}>
+            <div className={styles.title_logo}>
+              <i className="ri-list-check-3"></i>
+            </div>
+
+            <h1>
+              Mia<span>Task</span>
+            </h1>
           </div>
 
-          <h1>
-            Mia<span>Task</span>
-          </h1>
-        </div>
+          <div className={styles.add_icon}>
+            <i className="ri-add-circle-line"></i>
+          </div>
+        </header>
 
-        <div className={styles.add_icon}>
-          <i className="ri-add-circle-line"></i>
-        </div>
-      </header>
+        <main>main</main>
+
+        <footer>
+          <Link className="text_link" href="https://github.com/miaslls" target="_blank">
+            @miaslls
+          </Link>
+          <span>2023</span>
+
+          <span className={styles.separator_icon}>
+            <i className="ri-infinity-line"></i>
+          </span>
+
+          <Link
+            className="text_link"
+            href="https://fonts.google.com/specimen/Roboto+Mono"
+            target="_blank"
+          >
+            font
+          </Link>
+
+          <span className={styles.separator_icon}>
+            <i className="ri-infinity-line"></i>
+          </span>
+
+          <Link className="text_link" href="https://remixicon.com" target="_blank">
+            icons
+          </Link>
+        </footer>
+      </div>
     </>
   );
 }
