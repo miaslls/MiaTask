71df9991545a8b3d26bd148e67e10f545697ce86
miaslls <97785569+miaslls@users.noreply.github.com>
2023-09-27 22:52:12 -0300

    import font + icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import '@/styles/_reset.css';
 import '@/styles/globals.css';
-
 import type { AppProps } from 'next/app';
+import { Roboto_Mono } from 'next/font/google';
+
+const roboto_mono = Roboto_Mono({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --default-font: ${roboto_mono.style.fontFamily};
+          --default-light: 300;
+          --default-regular: 400;
+          --default-bold: 500;
+        }
+      `}</style>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
