73f13820287196e16e9e635f4efe733666e9aeb7
miaslls <97785569+miaslls@users.noreply.github.com>
2023-09-28 16:14:35 -0300

    style header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import styles from '@/styles/home.module.css';
 import Head from 'next/head';
 
 export default function Home() {
@@ -8,6 +9,21 @@ export default function Home() {
           MiaTask - Simple Task Management | Organize, Update, and Complete Tasks Effortlessly
         </title>
       </Head>
+      <header>
+        <div className={styles.title}>
+          <div className={styles.title_logo}>
+            <i className="ri-list-check-3"></i>
+          </div>
+
+          <h1>
+            Mia<span>Task</span>
+          </h1>
+        </div>
+
+        <div className={styles.add_icon}>
+          <i className="ri-add-circle-line"></i>
+        </div>
+      </header>
     </>
   );
 }
