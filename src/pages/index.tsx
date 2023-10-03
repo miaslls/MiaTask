760d3e012dd41045bb1cc917bb9e76ea0504fd5e
miaslls <97785569+miaslls@users.noreply.github.com>
2023-10-02 22:56:19 -0300

    implement create task

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,39 @@ import styles from '@/styles/home.module.css';
 
 import Head from 'next/head';
 import Link from 'next/link';
+import prisma from '@/lib/prisma';
+import { FormEvent, useState } from 'react';
 import { GetServerSideProps } from 'next';
 
-import prisma from '@/lib/prisma';
 import { Task } from '@prisma/client';
 import TaskItem from '@/components/task-item';
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const tasks = await prisma.task.findMany({
-    orderBy: [{ starred: 'desc' }, { completed: 'asc' }, { updatedAt: 'desc' }],
-  });
-
-  return { props: { tasks: JSON.parse(JSON.stringify(tasks)) } };
-};
-
 export default function Home({ tasks }: { tasks: Task[] }) {
+  const [tasklist, setTasklist] = useState(tasks);
+  const [showCreateForm, setShowCreateForm] = useState(false);
+  const [inputText, setInputText] = useState('');
+
+  async function submitCreateData(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setShowCreateForm(false);
+
+    try {
+      const response = await fetch('/api/create', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ text: inputText }),
+      });
+
+      const data = await response.json();
+      const indexToInsert = tasklist.findLastIndex((task) => task.starred) + 1;
+
+      setTasklist(tasklist.toSpliced(indexToInsert, 0, data.task));
+      setInputText('');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <>
       <Head>
@@ -36,26 +54,32 @@ export default function Home({ tasks }: { tasks: Task[] }) {
             </h1>
           </div>
 
-          <div className={styles.add_icon}>
-            <i className="ri-add-circle-line"></i>
+          <div className={styles.add_icon} onClick={() => setShowCreateForm(!showCreateForm)}>
+            <i className={showCreateForm ? 'ri-close-circle-line' : 'ri-add-circle-line'}></i>
           </div>
         </header>
 
         <main>
           <ul className={styles.tasklist}>
-            <li className={styles.task_input_wrapper}>
-              <input
-                type="text"
-                name="title"
-                className={styles.task_input}
-                placeholder="Type your task here..."
-              />
-              <span className={styles.task_input_icon}>
-                <i className="ri-arrow-right-s-line"></i>
-              </span>
-            </li>
+            {showCreateForm && (
+              <form className={styles.task_input_wrapper} onSubmit={submitCreateData}>
+                <input
+                  autoFocus
+                  required
+                  type="text"
+                  name="text"
+                  value={inputText}
+                  className={styles.task_input}
+                  placeholder="Type your task here..."
+                  onChange={(e) => setInputText(e.target.value)}
+                />
+                <button className={styles.task_input_icon} type="submit">
+                  <i className="ri-arrow-right-s-line"></i>
+                </button>
+              </form>
+            )}
 
-            {tasks.map((task) => (
+            {tasklist.map((task) => (
               <TaskItem task={task} key={`task-item-${task.id}`} />
             ))}
           </ul>
@@ -91,3 +115,11 @@ export default function Home({ tasks }: { tasks: Task[] }) {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const tasks = await prisma.task.findMany({
+    orderBy: [{ starred: 'desc' }, { completed: 'asc' }, { updatedAt: 'desc' }],
+  });
+
+  return { props: { tasks: JSON.parse(JSON.stringify(tasks)) } };
+};
