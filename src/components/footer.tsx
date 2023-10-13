e199397e817cd52afc187b04e58212716e3dff23
miaslls <97785569+miaslls@users.noreply.github.com>
2023-10-13 03:41:35 -0300

    footer link adjustments

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,41 +4,44 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer>
-      <Link
-        className="text_link"
-        href="https://github.com/miaslls"
-        target="_blank"
-        title="Author's Github page"
-      >
-        @miaslls
-      </Link>
-      <span>2023</span>
+      <span className={styles.footer_link}>
+        <Link
+          className="text_link"
+          href="https://github.com/miaslls"
+          target="_blank"
+          title="External link: Author's Github page"
+        >
+          @miaslls
+        </Link>
 
-      <span className={styles.separator_icon}>
-        <i className="ri-infinity-line"></i>
+        <i className="ri-external-link-line"></i>
       </span>
+      ৹
+      <span className={styles.footer_link}>
+        <Link
+          className="text_link"
+          href="https://fonts.google.com/specimen/Roboto+Mono"
+          target="_blank"
+          title="External link: Font-family used in the project: Google's Roboto Mono"
+        >
+          font
+        </Link>
 
-      <Link
-        className="text_link"
-        href="https://fonts.google.com/specimen/Roboto+Mono"
-        target="_blank"
-        title="Font-family used in the project: Google's Roboto Mono"
-      >
-        font
-      </Link>
-
-      <span className={styles.separator_icon}>
-        <i className="ri-infinity-line"></i>
+        <i className="ri-external-link-line"></i>
       </span>
+      ৹
+      <span className={styles.footer_link}>
+        <Link
+          className="text_link"
+          href="https://remixicon.com"
+          target="_blank"
+          title="External link: Icon system used in the project: Remix Icon"
+        >
+          icons
+        </Link>
 
-      <Link
-        className="text_link"
-        href="https://remixicon.com"
-        target="_blank"
-        title="Icon system used in the project: Remix Icon"
-      >
-        icons
-      </Link>
+        <i className="ri-external-link-line"></i>
+      </span>
     </footer>
   );
 }
