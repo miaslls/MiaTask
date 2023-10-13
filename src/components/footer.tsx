3a3a501ca9408e73c357d39296f3786bd3757ba6
miaslls <97785569+miaslls@users.noreply.github.com>
2023-10-13 02:41:33 -0300

    WCAG SC 2.4.4

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,12 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer>
-      <Link className="text_link" href="https://github.com/miaslls" target="_blank" tabIndex={0}>
+      <Link
+        className="text_link"
+        href="https://github.com/miaslls"
+        target="_blank"
+        title="Author's Github page"
+      >
         @miaslls
       </Link>
       <span>2023</span>
@@ -17,7 +22,7 @@ export default function Footer() {
         className="text_link"
         href="https://fonts.google.com/specimen/Roboto+Mono"
         target="_blank"
-        tabIndex={0}
+        title="Font-family used in the project: Google's Roboto Mono"
       >
         font
       </Link>
@@ -26,7 +31,12 @@ export default function Footer() {
         <i className="ri-infinity-line"></i>
       </span>
 
-      <Link className="text_link" href="https://remixicon.com" target="_blank" tabIndex={0}>
+      <Link
+        className="text_link"
+        href="https://remixicon.com"
+        target="_blank"
+        title="Icon system used in the project: Remix Icon"
+      >
         icons
       </Link>
     </footer>
