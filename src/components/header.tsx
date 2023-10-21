d3b23211497e4aa2de33086982fa62a5e9d171ef
miaslls <97785569+miaslls@users.noreply.github.com>
2023-10-21 03:23:54 -0300

    toggle between dark and light themes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
 import styles from './styles/header.module.css';
+import { useTheme } from './context/theme-provider';
 
 export default function Header() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <header>
       <div className={styles.title}>
@@ -12,6 +15,16 @@ export default function Header() {
           Mia<span>Task</span>
         </h1>
       </div>
+
+      {/* 🔻 component and styles adapted from https://webtips.dev/toggle-buttons-in-react ❗ */}
+
+      <label className={styles.toggle_theme_button}>
+        <input type="checkbox" defaultChecked={theme === 'dark'} onClick={toggleTheme} />
+        <span></span>
+        <strong>
+          <i className="ri-moon-line"></i>
+        </strong>
+      </label>
     </header>
   );
 }
