6868aa53e39c48c1dc28393132feaac90cce22ba
miaslls <97785569+miaslls@users.noreply.github.com>
2023-10-28 13:35:21 -0300

    link page title to home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,22 @@
 import styles from './styles/header.module.css';
+
+import Link from 'next/link';
+import useTranslation from 'next-translate/useTranslation';
 import ToggleThemeButton from './toggle-theme-button';
 
 export default function Header() {
+  // const { locale: currentLocale } = useRouter();
+  const { t, lang } = useTranslation();
+
   return (
     <header>
-      <div className={styles.title}>
+      <Link
+        className={styles.title}
+        href="/"
+        locale={lang}
+        aria-label={t('a11y:aria.label.go-home')}
+        title={t('a11y:title.home')}
+      >
         <div className={styles.title_logo}>
           <i className="ri-list-check-3"></i>
         </div>
@@ -12,7 +24,7 @@ export default function Header() {
         <h1>
           Mia<span>Task</span>
         </h1>
-      </div>
+      </Link>
 
       <ToggleThemeButton />
     </header>
