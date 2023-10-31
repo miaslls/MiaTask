a081156603e1c732b56668b6fa72ede120a6f3b2
miaslls <97785569+miaslls@users.noreply.github.com>
2023-10-31 01:02:00 -0300

    extract `toggleTaskAction(params)` to lib/actions

--- src/components/task-list/task-item.tsx
+++ src/components/task-list/task-item.tsx
@@ -1,53 +1,52 @@
 import styles from './styles/task-item.module.css';
 
-import { mutate } from 'swr';
-import { toast } from 'react-hot-toast';
-import { dismissableErrorToast } from '@src/lib/toast';
 import { extendTask } from '@src/lib/format';
+import { toggleTaskAction } from '@src/lib/actions';
 import useTranslation from 'next-translate/useTranslation';
 import useDeviceOrientation from '@hooks/useDeviceOrientation';
 
 import type { Task } from '@prisma/client';
 import type { ExtendedTask, OpenElement } from '@src/pages/index';
+import type { ToggleTaskActionParams } from '@src/lib/actions';
 
-export type ToggleTaskActionParams = {
-  id: string;
-  action: 'complete' | 'star';
-  translate: CallableFunction;
-  lang: string;
-};
-
-// extract to lib/actions ❓
-export async function toggleTaskAction({
-  id,
-  action,
-  translate,
-  lang,
-}: ToggleTaskActionParams): Promise<ExtendedTask | null> {
-  const toastId = toast.loading(translate('loading'));
-
-  const tasklist = '/api/task';
-  const key = `${tasklist}/${id}/${action}`;
-
-  const response = await fetch(key, {
-    method: 'PATCH',
-    headers: { 'Accept-Language': lang },
-  });
-
-  if (response.ok) {
-    toast.success(translate('done'), { id: toastId });
-    mutate([tasklist, lang]);
-  } else {
-    const error = await response.json();
-    dismissableErrorToast(error.message);
-    toast.dismiss(toastId);
-    return null;
-  }
-
-  const responseTask = await response.json().then((r) => r.task);
-
-  return extendTask(responseTask, lang);
-}
+// export type ToggleTaskActionParams = {
+//   id: string;
+//   action: 'complete' | 'star';
+//   translate: CallableFunction;
+//   lang: string;
+// };
+//
+// // extract to lib/actions ❓
+// export async function toggleTaskAction({
+//   id,
+//   action,
+//   translate,
+//   lang,
+// }: ToggleTaskActionParams): Promise<ExtendedTask | null> {
+//   const toastId = toast.loading(translate('loading'));
+//
+//   const tasklist = '/api/task';
+//   const key = `${tasklist}/${id}/${action}`;
+//
+//   const response = await fetch(key, {
+//     method: 'PATCH',
+//     headers: { 'Accept-Language': lang },
+//   });
+//
+//   if (response.ok) {
+//     toast.success(translate('done'), { id: toastId });
+//     mutate([tasklist, lang]);
+//   } else {
+//     const error = await response.json();
+//     dismissableErrorToast(error.message);
+//     toast.dismiss(toastId);
+//     return null;
+//   }
+//
+//   const responseTask = await response.json().then((r) => r.task);
+//
+//   return extendTask(responseTask, lang);
+// }
 
 export type TaskItemProps = {
   task: Task;
--- src/components/task-modal/options-nav.tsx
+++ src/components/task-modal/options-nav.tsx
@@ -1,15 +1,21 @@
-import styles from './styles/options-nav.module.css';
+import styles from './styles/modal-element.module.css';
 
 import useTranslation from 'next-translate/useTranslation';
 import type { ExtendedTask } from '@src/pages/index';
-import type { ToggleTaskActionParams } from '@components/task-list/task-item';
+import type { ToggleTaskActionParams } from '@src/lib/actions';
+import type { OpenModalElement } from '@components/task-modal';
 
 export type OptionsNavProps = {
   task: ExtendedTask;
   handleModalAction(params: ToggleTaskActionParams): void;
+  handleOpenModalElement(element: OpenModalElement): void;
 };
 
-export default function OptionsNav({ task, handleModalAction }: OptionsNavProps) {
+export default function OptionsNav({
+  task,
+  handleModalAction,
+  handleOpenModalElement,
+}: OptionsNavProps) {
   const { t, lang } = useTranslation();
 
   return (
@@ -27,16 +33,7 @@ export default function OptionsNav({ task, handleModalAction }: OptionsNavProps)
       <button
         type="button"
         className={styles.button + ' ' + styles.nav_button}
-        onClick={() => handleModalAction({ id: task.id, action: 'star', translate: t, lang })}
-        aria-label={t('a11y:aria.label.star')}
-        title={t('a11y:title.star')}
-      >
-        <i className={task.starred ? 'ri-star-fill' : 'ri-star-line'}></i>
-      </button>
-
-      <button
-        type="button"
-        className={styles.button + ' ' + styles.nav_button}
+        onClick={() => handleOpenModalElement('update')}
         aria-label={t('a11y:aria.label.edit')}
         title={t('a11y:title.edit')}
       >
@@ -46,10 +43,21 @@ export default function OptionsNav({ task, handleModalAction }: OptionsNavProps)
       <button
         type="button"
         className={styles.button + ' ' + styles.nav_button}
+        onClick={() => handleOpenModalElement('delete')}
         aria-label={t('a11y:aria.label.delete')}
         title={t('a11y:title.delete')}
       >
         <i className="ri-delete-bin-2-line"></i>
+      </button>
+
+      <button
+        type="button"
+        className={styles.button + ' ' + styles.nav_button}
+        onClick={() => handleModalAction({ id: task.id, action: 'star', translate: t, lang })}
+        aria-label={t('a11y:aria.label.star')}
+        title={t('a11y:title.star')}
+      >
+        <i className={task.starred ? 'ri-star-fill' : 'ri-star-line'}></i>
       </button>
     </nav>
   );
