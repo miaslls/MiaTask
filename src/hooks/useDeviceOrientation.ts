676f40dd00164cd2930063d5f4fc3712ca7556c6
miaslls <97785569+miaslls@users.noreply.github.com>
2023-11-03 11:36:30 -0300

    remove event listener

--- src/hooks/useDeviceOrientation.ts
+++ src/hooks/useDeviceOrientation.ts
@@ -6,23 +6,13 @@ export default function useDeviceOrientation() {
   const [orientation, setOrientation] = useState<Orientation | undefined>();
 
   useEffect(() => {
-    const queryString = window.matchMedia('(orientation: landscape)');
+    const query = window.matchMedia('(orientation: landscape)');
 
-    function handleOrientation(query: MediaQueryList | MediaQueryListEvent) {
-      if (query.matches) {
-        setOrientation('landscape');
-      } else {
-        setOrientation('portrait');
-      }
+    if (query.matches) {
+      setOrientation('landscape');
+    } else {
+      setOrientation('portrait');
     }
-
-    handleOrientation(queryString);
-
-    queryString.addEventListener('change', handleOrientation);
-
-    return () => {
-      queryString.removeEventListener('change', handleOrientation);
-    };
   }, []);
 
   return orientation;
